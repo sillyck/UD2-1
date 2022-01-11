08981ac851144f7bfc52ddff45939cf2df249dbe
sillyck <ribellas2002@gmail.com>
2022-01-11 12:40:13 +0100

    hola

--- ComentarioLinea.java
+++ ComentarioLinea.java
@@ -1,6 +1,6 @@
 public class ComentarioLinea {
 	public static void main(String[] args){
-		//System.out.println("HOla");
+		//System.out.println("HOla"); 
 		System.out.println("Adios");
 		System.out.println("FIN DEL PROGRAMA");
 	}
